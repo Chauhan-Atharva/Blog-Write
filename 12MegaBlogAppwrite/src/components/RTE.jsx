093c36247857d1b9294b5425849f1318b469a8a1
nick <atharva.st.05@gmail.com>
2026-05-10 23:37:22 +0530

    vercel tinymce issue fixed

--- 12MegaBlogAppwrite/src/components/RTE.jsx
+++ 12MegaBlogAppwrite/src/components/RTE.jsx
@@ -12,7 +12,7 @@ export default function RTE({name,control,label,defaultValue = ""}){
             control={control}
             render = {({field: {onChange}})=> (
                 <Editor
-            apiKey="fxtebwbxqa1nnh0k2jbqxxzxdqultavwso75gtr4p2hwz229"
+            apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
             initialValue={defaultValue}
             init={{
             initialValue: defaultValue,
